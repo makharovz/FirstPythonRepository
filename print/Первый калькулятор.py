6c085e95460e6b5f24330a5d181e873c2b93854b
Zaur Makharov <zmakharov@aflt-systems.ru>
2024-07-30 18:00:42 +0300

    Написал первый калькулятор

---
+++ print/Первый калькулятор.py
@@ -0,0 +1,9 @@
+print("Мой первый калькулятор")
+num1 = input("Введите первое число: ")
+num2 = input("Введите второе число: ")
+
+num1 = int(num1)
+num2 = int(num2)
+
+print("Ответ: ")
+print(num1 + num2)
