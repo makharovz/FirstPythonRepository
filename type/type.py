801dce4c8549bcdf748caf3b4cc2659651b0e049
Zaur Makharov <zmakharov@aflt-systems.ru>
2024-08-05 16:37:16 +0300

    Добавил файл type

---
+++ type/type.py
@@ -0,0 +1,28 @@
+var_1 = 1, 5, 10, 15
+# Целое число
+
+var_2 = 1.5, 5.5, 10.3, 15.9
+# Число с плавающей точкой (запятой)
+
+var_3 = "Hello, World!"
+# Строка
+
+var_4 = False
+# Истина/Ложь
+
+var_5 = [1, 2, 3, 4, 5]
+# Список
+
+var_6 = {"Ivan", "Oleg", "Zaur"}
+# Множества
+
+var_7 = {"Name":"Zaur", "Age":"27", "Gender":"Male"}
+# Словарь
+
+print(type(var_1))
+print(type(var_2))
+print(type(var_3))
+print(type(var_4))
+print(type(var_5))
+print(type(var_6))
+print(type(var_7))
